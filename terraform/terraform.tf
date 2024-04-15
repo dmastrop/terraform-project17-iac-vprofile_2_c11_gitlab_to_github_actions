dd379eab5215fdfab68ec5f7c98b15a22a3212a1
David Mastropolo <dave.mastropolo@gmail.com>
2024-04-15 16:57:28 -0700

    terraform version

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -40,8 +40,8 @@ terraform {
   }
 
   # this is the miniumum version of terraform to use
-  #required_version = "~> 1.6.3"
-  required_version = 1.6.6
+  required_version = "~> 1.6.3"
+  #required_version = 1.6.6
 }
 ##
 ##
