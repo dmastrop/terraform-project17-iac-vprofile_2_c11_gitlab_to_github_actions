9bf20566614df2b73ebd335594f5622431882efb
David Mastropolo <dave.mastropolo@gmail.com>
2025-01-25 16:20:27 -0800

    try creating a terraform EKS cluster with version 1.30 standard support push to origin2 for gitlab to github

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -6,7 +6,8 @@ module "eks" {
 
   cluster_name = local.cluster_name
   # see main.tf file for locals block
-  cluster_version = "1.27"
+  #cluster_version = "1.27"
+  cluster_version = "1.30"
 
   vpc_id = module.vpc.vpc_id
   # this is from the vpc.tf file
